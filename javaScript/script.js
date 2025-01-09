f0c53eea93d2da50601a368d1380406cfcf2f717
-S <npshunxiang@gmail.com>
2025-01-09 22:58:58 +0800

    added js script to store items in the constant

---
+++ javaScript/script.js
@@ -0,0 +1,9 @@
+
+
+
+const products = [
+    { name: "Laptop", category: "electronics", price: 800, condition: "new" },
+    { name: "Sofa", category: "furniture", price: 1200, condition: "second-hand" },
+
+
+  ];
